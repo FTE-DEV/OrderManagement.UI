9752ae675a26bcab5548a4088a484fbb94e65259
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2023-05-16 04:43:52 +0200

    Form tags implemented

--- src/app/components/customers/customer/customer.component.ts
+++ src/app/components/customers/customer/customer.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Customer } from 'src/app/interfaces/customer.intertface';
 import { CustomersService } from 'src/app/services/customers-service.service';
+import { FormBuilder, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-customer',
@@ -20,12 +21,20 @@ export class CustomerComponent {
     postalCode: "",
     city:""
   };
+
+
+  
+
+
   id : number;
   routeSubscription : any;
 
-  constructor(private customerService:CustomersService, private route: ActivatedRoute)
-  {}
+  constructor(private customerService:CustomersService, private route: ActivatedRoute, private formBuilder:FormBuilder)
+  {
+    
+  }
 
+  
   ngOnInit()
   {
     this.routeSubscription = this.route.params.subscribe(params => {
